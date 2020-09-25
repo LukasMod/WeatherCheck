9cf6f083680b35e7404ff8fd8b1e574d287e8dfc
Lukasz Modzelewski <lukaszmod@o2.pl>
2020-09-25 08:46:16 +0200

    Updates

---
+++ precache-manifest.ea00b9cb1e9bf9adff87fcc1942cb2fb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c85b940cbc79630208f0",
+    "url": "/WeatherCheck/static/css/main.e6d9bc06.chunk.css"
+  },
+  {
+    "revision": "c85b940cbc79630208f0",
+    "url": "/WeatherCheck/static/js/main.c85b940c.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/WeatherCheck/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "329062ed1aa0dc639d4c",
+    "url": "/WeatherCheck/static/js/runtime~main.329062ed.js"
+  },
+  {
+    "revision": "3984697fff6e29485277c8693c458e27",
+    "url": "/WeatherCheck/index.html"
+  }
+];
